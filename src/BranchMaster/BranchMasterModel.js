037a21a61d9d76154283c8c6fcb659b92a3a0c1f
Dipakpatel787 <dipakpatel8055@gmail.com>
2024-02-05 18:05:19 +0530

    branchmaster schema commented

--- src/BranchMaster/BranchMasterModel.js
+++ src/BranchMaster/BranchMasterModel.js
@@ -1,23 +1,23 @@
-const mongoose = require('mongoose')
-//const docUpload =require('../docUpload/docUploadModel');
-const { ObjectId } = require('mongodb');
-const branchMasterSchema =mongoose.Schema(
-    {
+// const mongoose = require('mongoose')
+// //const docUpload =require('../docUpload/docUploadModel');
+// const { ObjectId } = require('mongodb');
+// const branchMasterSchema =mongoose.Schema(
+//     {
 
-    oid:           	{ type: String },
-    Branch:         	{ type: String },
-PWD:{type:String}
+//     oid:           	{ type: String },
+//     Branch:         	{ type: String },
+// PWD:{type:String}
    
 
 
-    },
-    {
-        timestamps : true
-    }
+//     },
+//     {
+//         timestamps : true
+//     }
 
-)
+// )
 
 
-const BranchMaster = mongoose.model('BranchMaster', branchMasterSchema);
+// const BranchMaster = mongoose.model('BranchMaster', branchMasterSchema);
 
-module.exports =BranchMaster;
+// module.exports =BranchMaster;
